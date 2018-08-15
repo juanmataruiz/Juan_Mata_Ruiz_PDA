0e9e977ec8c6c9d5a4fd65a9533b2dbde3bbef27
Juan Mata Ruiz <juanmataruiz84@gmail.com>
2018-08-15 12:16:57 +0100

    static and dynamic tests pass

--- Static_and_Dynamic_TaskA/specs/dynamic_testing_spec.rb
+++ Static_and_Dynamic_TaskA/specs/dynamic_testing_spec.rb
@@ -2,35 +2,30 @@
     require_relative '../dynamic_testing.rb'
     require_relative '../card.rb'
     require 'minitest/autorun'
-    require('minitest/rg')
+    # require('minitest/rg')
 
     class TestCard < MiniTest::Test
 
       def setup
-        @card1 = Card.new("One", 1)
-        @card2 = Card.new("Two", 2)
+        @card1 = Card.new("Ace", 1)
+        @card2 = Card.new("Joker", 0)
         @card_game = CardGame.new
-        @cards = [@card1, @card2]
       end
 
       def test_check_for_ace__True
-        assert_equal(false, @card_game.check_for_ace(@card1))
+        assert_equal(true, @card_game.check_for_ace(@card1))
       end
 
       def test_check_for_ace__False
-        assert_equal(true, @card_game.check_for_ace(@card2))
+        assert_equal(false, @card_game.check_for_ace(@card2))
       end
 
       def test_highest_card
-        assert_equal(3, @card_game.highest_card(@card2, @card1))
+        assert_equal(1, @card_game.highest_card(@card2, @card1))
       end
 
       def test_cards_total
-        assert_equal("You have a total of 3", CardGame.cards_total(@cards))
+        assert_equal("You have a total of 1", CardGame.cards_total([@card1]))
       end
     end
 
-
-
-
-#n
